9dadabf226d9a33f4a3123317f7a2f8444225de9
dc-turnbullo <turnbullo@dulwich.org.uk>
2024-09-10 13:29:53 +0100

    prep1

---
+++ Programming/Programming_basics/prep1q1.py
@@ -0,0 +1,4 @@
+Pounds = float(input("enter pounds"))
+Kilos = float(input("enter kilos"))
+print("pounds:", Pounds / 2.2)
+print("kilos:", Kilos * 2.2)
---
+++ Programming/Programming_basics/prep1q2.py
@@ -0,0 +1,2 @@
+num = str(input("Enter a three digit number"))
+print(num[0], "hundreds,", num[1], "tens and", num[2], "units")
