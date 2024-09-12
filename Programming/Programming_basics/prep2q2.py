ebe9b01d896f2010c6c576069a5212af62cf47bd
dc-turnbullo <turnbullo@dulwich.org.uk>
2024-09-12 11:58:27 +0100

    prep2q2

---
+++ Programming/Programming_basics/prep2q2.py
@@ -0,0 +1,10 @@
+temp = int(input("enter temperature"))
+humidity = int(input("enter humidity"))
+window = input("is the window opened or closed? ")
+if ((temp > 25) or (humidity > 50)) and (window == "closed"):
+	print("open the window")
+elif ((temp < 10) and (humidity < 50)) and (window == "open"):
+	print("close the window")
+else:
+	print("everything is good!")
+	
