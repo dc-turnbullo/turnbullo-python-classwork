5b2be081f92402ceddb7f079f0e2d58926dd2e27
dc-turnbullo <turnbullo@dulwich.org.uk>
2024-09-13 13:42:55 +0100

    pygametest

---
+++ pygame_practice/pygametest.py
@@ -0,0 +1,15 @@
+import pygame
+
+
+background_colour = (234, 212, 252) 
+screen = pygame.display.set_mode((300, 300)) 
+pygame.display.set_caption('Gomez is short') 
+screen.fill(background_colour) 
+pygame.display.flip() 
+running = True
+while running: 
+    for event in pygame.event.get():      
+        if event.type == pygame.QUIT: 
+            running = False
+    
+    pygame.draw.rect(10)
