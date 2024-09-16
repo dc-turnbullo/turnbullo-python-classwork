684a0993a62ad4ec1b020ace09583dcf4d9c6feb
Steven Carroll <32237935+dc-carrolls@users.noreply.github.com>
2024-09-16 09:53:43 +0100

    Feedback: Selection Prep
    Make sure you are using the coding conventions.

--- Programming/Programming_basics/prep2q2.py
+++ Programming/Programming_basics/prep2q2.py
@@ -1,3 +1,4 @@
+### SRC - Please make sure you are using the coding conventions, i.e. #end if
 temp = int(input("enter temperature"))
 humidity = int(input("enter humidity"))
 window = input("is the window opened or closed? ")
@@ -7,4 +8,5 @@ elif ((temp < 10) and (humidity < 50)) and (window == "open"):
 	print("close the window")
 else:
 	print("everything is good!")
+#end if
 	
