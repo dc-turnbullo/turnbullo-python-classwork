beb1f667d9bfa0b6dce209ce89bb78a41121eb84
dc-turnbullo <turnbullo@dulwich.org.uk>
2024-09-19 21:36:24 +0100

    last iteration

--- iteration.py
+++ iteration.py
@@ -5,10 +5,16 @@
 #next i
 #print(i)
 
-z = 1
-while True:
-    table = z*5
-    print(z,"times by 5 equals", table)
-    z+=1
-    if z > 12:
-        break
+#z = 1
+#while True:
+    #table = z*5
+    #print(z,"times by 5 equals", table)
+    #z+=1
+    #if z > 12:
+        #break
+
+#j = 1
+#while j <= 12:
+    #table = j *5
+    #print(j,"times by 5 equals",table)
+    #j+=1
