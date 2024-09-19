b87e25831b16e55e00b1febe55232b402431808f
dc-turnbullo <turnbullo@dulwich.org.uk>
2024-09-19 10:41:09 +0100

    iterations

---
+++ iteration.py
@@ -0,0 +1,14 @@
+#tables = ""
+#for i in range(0,13):
+    #table = i*5
+    #print(i ," times by 5 equals", table)
+#next i
+#print(i)
+
+z = 1
+while True:
+    table = z*5
+    print(z,"times by 5 equals", table)
+    z+=1
+    if z > 12:
+        break
