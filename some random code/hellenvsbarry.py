b0325a4f493e4d427d2cc71d8ba2ba64a761a77b
dc-turnbullo <turnbullo@dulwich.org.uk>
2024-10-08 09:54:18 +0100

    something to do with brry and hellen

---
+++ some random code/hellenvsbarry.py
@@ -0,0 +1,8 @@
+
+presents = [("A","B","C","D","E")("F","G","H","I","J")("K","L","M","N","O")]
+barry = []
+hellen = ""
+hellen = hellen + presents[0,0]
+barry = barry + presents
+print("Barry got",barry)
+print("hellen got",hellen)
