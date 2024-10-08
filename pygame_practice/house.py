f8f107630e28f0303e6d1edb61c5efd7506c7097
dc-turnbullo <turnbullo@dulwich.org.uk>
2024-10-08 10:30:23 +0100

    house

---
+++ pygame_practice/house.py
@@ -0,0 +1,33 @@
+import pygame
+pygame.init()
+
+size = (700, 400)
+screen = pygame.display.set_mode(size)
+done = False
+clock = pygame.time.Clock()
+fps = 60
+white = (0xff, 0xff, 0xff)
+red = (0xff, 0, 0)
+green = (0x00,0xff,0x00)
+brown = (0x96,0x4b,0x00)
+yellow = (0xff,0xff,0x00)
+pi = 3.141592658772397170284017237109779827308099865209818986239560981203791639817291838918747298374271798237091734798120779326918273012798366409124017598661030830912097598237981740973209175208168926387091259816629836981279010984091298446932650917092381569816239871092570162396129871092830912896561298709185798236951907928380165981263097109871962498173095701973216487638974091203971057983261912739817240971072309850723094810928857098218307185371098238128985781927093838582734712893801
+pygame.display.set_caption("Is he short or is he not short")
+rtx = 55
+rty = 50
+brx = 20
+bry = 25
+movement= 10
+movementy = 10
+while not done:
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
+            done = True 
+    clock.tick(fps)
+    screen.fill(white)
+    pygame.draw.rect(screen, red, [280, 300, 100, 140], 0)
+    pygame.draw.polygon(screen, green, ([280,300],[330,280] ,[380,300]))
+    pygame.draw.rect(screen, brown, [325, 350, 30, 50], 0)
+    pygame.draw.circle(screen, yellow, (50, 50), 20, 0) 
+
+    pygame.display.flip()
