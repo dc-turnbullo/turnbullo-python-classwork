6d01749276ff039b52c0b84d40568e932f533bc5
dc-turnbullo <turnbullo@dulwich.org.uk>
2024-10-14 10:46:30 +0100

    pongington

--- pygame_practice/Pong.py
+++ pygame_practice/Pong.py
@@ -30,6 +30,11 @@ p2tlx = 670
 p2tly = 150
 p2xlen = 20
 p2ylen = 70
+def resetball(ballspeed,ballx,bally):
+    ballspeed = 1
+    ballx = 350
+    bally = 200
+    return ballspeed,ballx,bally
 
 def p2movedown(p2tly):
     p2tly+=5
@@ -74,12 +79,12 @@ while not done:
     bally = bally + (ballspeed * balldiry)
     p1tly = p1tly + (ballspeed * balldiry)
     
-    if (ballx >= p2tlx and ballx<= p2tlx + p2xlen) and (bally >= p2tly and bally <= p2tly + p2ylen):
+    if ((ballx+20) >= p2tlx and (ballx+20)<= p2tlx + p2xlen) and (bally >= p2tly and bally <= p2tly + p2ylen):
         balldirx = balldirx * -1
         ballspeed = ballspeed + 1
         print("ball touched paddel")
     
-    if (ballx >= p1tlx and ballx<= p1tlx + p1xlen) and (bally >= p1tly and bally <= p1tly + p1ylen):
+    if ((ballx-20) >= p1tlx and (ballx-20)<= p1tlx + p1xlen) and (bally >= p1tly and bally <= p1tly + p1ylen):
         balldirx = balldirx * -1
         ballspeed = ballspeed + 1
         print("ball touched paddel")
@@ -87,11 +92,27 @@ while not done:
     
     if bally <=0 or bally >= 400:
         balldiry *= -1
-        touched = False
+        
     
-    text = font.render("Player 1 score: ", p1score, True, red)
-    text = font.render("Player 2 score: ", p2score, True, blue)
+    text = font.render("Player 1 score: "+ str(p1score), p1score, True, red)
+    text2 = font.render("Player 2 score: " + str(p2score), p2score, True, blue)
 
+    screen.blit(text2, [500, 30])
+    screen.blit(text, [30, 30])
+
+    if ballx > 700:
+        p1score +=1
+        ballspeed,ballx,bally = resetball(ballspeed,ballx,bally)
+        p1tlx = 10
+        p1tly = 150
+        
+
+    
+    if ballx<0:
+        p2score +=1
+        ballspeed,ballx,bally = resetball(ballspeed,ballx,bally)
+        p1tlx = 10
+        p1tly = 150
 
     pygame.display.flip()
 
