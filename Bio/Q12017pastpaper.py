ee535395ac52fbb49f4bca4a564d0cbcef56b887
dc-turnbullo <turnbullo@dulwich.org.uk>
2024-12-09 10:45:26 +0000

    bio something or toher

--- Bio/Q12017pastpaper.py
+++ Bio/Q12017pastpaper.py
@@ -1,14 +1,17 @@
 row = str(input("enter the things in the first row"))
+row = row.upper()
 lob = len(row)
 counter = lob-1
 triangle = []
 def checkdiff(input1,input2):
-    if (input1 == "r" or input2 == "r") and (input2 == "g" or input1 == "g"):
-        return "b"
-    elif input1 == (input1 == "r" or input2 == "r") and (input2 == "b" or input1 == "b"):
-        return "g"
-    else:
-        return "r"
+    input1 = input1.upper()
+    input2 = input2.upper()
+    if (input1 == "R" or input2 == "R") and (input2 == "G" or input1 == "G"):
+        return "B"
+    elif (input1 == "R" or input2 == "R") and (input2 == "B" or input1 == "B"):
+        return "G"
+    elif (input1 == "G" or input2 == "G") and (input2 == "B" or input1 == "B"):
+        return "R"
 
 
 for i in range(1,lob+1):
@@ -25,6 +28,9 @@ while counter >0:
             triangle[counter-1][subcounter] = triangle[counter][subcounter]
         else:
             triangle[counter-1][subcounter] = checkdiff(triangle[counter][subcounter],triangle[counter][subcounter+1])
-        print(triangle)
+        subcounter+=1
     counter -=1
-print(triangle)
+for i in range(lob):
+    print(triangle[i])
+
+print(triangle[0][0])
