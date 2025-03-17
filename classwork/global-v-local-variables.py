60c67ab9b000b5c8a794764d21ab8d76792e53fa
dc-turnbullo <turnbullo@dulwich.org.uk>
2025-03-17 10:22:46 +0000

    Update global-v-local-variables.py

--- classwork/global-v-local-variables.py
+++ classwork/global-v-local-variables.py
@@ -3,13 +3,13 @@ count = 0
 num = 0
 lowest = 1000000000
 while lowest != 1:
-    count = 0
+    
     while num != 63:
         num = random.randint(0, 100)
         count +=1
     print("attempt total =", count)
     if count < lowest:
         lowest = count
-
+    count = 0
         
 print("number of runs needed to get 63 on first attempt =", lowest)
